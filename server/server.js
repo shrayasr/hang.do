1e78bb02e80b09ac2a17b239b711dab93847d13a
vk <krthkv26@gmail.com>
2013-09-08 20:32:41 +0530

    add POST and GET id requests save to mongo

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 var express = require('express'),
-foos = require('./routes/places');
+places = require('./routes/places');
  
 var app = express();
- 
-app.get('/places', foos.findAll);
-app.get('/places/:id', foos.findById);
+app.use(express.bodyParser());
+
+app.get('/places', places.findAll);
+app.get('/places/:id', places.findById);
+app.post('/places',places.addPlace);
  
 app.listen(3000);
 console.log('Listening on port 3000...');
