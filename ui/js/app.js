3483a6de780a1d37312d38f6a03b8c8b56c32052
Subhash A <s.a@sap.com>
2013-09-22 19:41:06 +0530

    FInal commit with UI changes.

--- ui/js/app.js
+++ ui/js/app.js
@@ -5,27 +5,36 @@ YUI().use("node","model","view","model-list",function(Y){
     create_suggestion_list(Y);
     create_suggestion_view(Y);
     create_suggestion_view_list(Y);
+    var callService = function(e) {
+	var location = Y.one("#my-loc").get("value");
+	if(location.length !== 0 && location !== " ") {
+	    search_suggestions(Y,location);
+	}
+    }
+    Y.one("#kaboom").on("click",callService);
+    
+});
+
+function search_suggestions(Y,location) {
     var xlist = new Y.suggestion_list();
     xlist.sync("kormangala");
-});
+    var list_view = new Y.suggestion_view_list({"modellist":xlist,"srcNode":"#suggestions-container"});
+    list_view.render();
+    list_view.update();
+}
 
 function create_suggestion_model(Y) {
     var procs = {
 	"parse":function(data) {
 	    return new Y.suggestion_submodel(data);
-	},
-	"add":function(e) {
-	    console.log(e);
 	}
+	
     };
     var attrs = {
 	"movie":{
 	    "value":null
 	},
 	"food":{
-	    "value":null
-	},
-	"approx_budget":{
 	    "value":null
 	}
     };
@@ -38,10 +47,10 @@ function create_suggestion_submodel(Y) {
 	"name":{
 	    "value":null
 	},
-	"location":{
+	"location_specific":{
 	    "value":null
 	},
-	"address":{
+	"location_coarse":{
 	    "value":null
 	},
 	"cost":{
@@ -52,6 +61,9 @@ function create_suggestion_submodel(Y) {
 	},
 	"rating":{
 	    "value":null
+	},
+	"time":{
+	    "value":null
 	}
     };
     var suggestion_submodel = Y.suggestion_submodel = new Y.Base.create("suggestion_model",Y.Model,[],procs,{"ATTRS":attrs});
@@ -61,18 +73,20 @@ function create_suggestion_list(Y) {
     var procs = {
 	"model":Y.suggestion_model,
 	"sync":function(location) {
-	    list = this,
+	    var list = this,
 	    load = function(response){
-		list.add(response);
+		list.add(response.suggestions);
 		console.log("list synced");
-	    };
+		//console.log(response);
+	    },
 	    failed = function(xhr) {
 		console.log("call failed");
 	    };
 
 	    $.ajax({
-		"url":"suggestions.json",
+		"url":encodeURI("suggestions.json"),
 		"type":"GET",
+		"dataType":"json",
 		"async":true,
 		"success":load,
 		"error":failed
@@ -93,8 +107,41 @@ function create_suggestion_view(Y) {
 	"render":function() {
 	    var container = this.get("container"),
 	    model = this.get("model"),
-	    content = Y.Lang.sub(this.template,{"suggestion":model.get("plan")});
-	    
+	    subs_json = {
+		"m_name":model.get("movie").name,
+		"m_location":model.get("movie").name_extra,
+		"m_time":model.get("movie").time,
+		"r_name":model.get("food").name,
+		"r_location":model.get("food").location_coarse,
+		"appx_cost":parseInt(model.get("food").cost,10)+parseInt(model.get("movie").cost,10)
+		
+	    },
+	    content = Y.Lang.sub(this.template,subs_json),
+	    index = model.get("clientId").substring(model.get("clientId").length-1,model.get("clientId").length);
+	    container.one('.accordion-inner').setContent(content);
+	    container.one(".loc-image").set("src",this.getMapUrl(model));
+	    container.one('.accordion-toggle').setContent("Hangout Idea #"+index);
+	    return this;
+	},
+	"getMapUrl":function(model) {
+	    var url = "http://maps.googleapis.com/maps/api/staticmap?",
+	    map_center = "center="+model.get("movie").location_coarse,
+	    map_size="&size=600x300",
+	    map_type="&maptype=roadmap",
+	    map_markers = this.getMarkers(model),
+	    map_sensor = "&sensor=false";
+	    //console.log(url+map_center+map_size+map_type+map_markers+map_sensor);
+	    return url+map_center+map_size+map_type+map_markers+map_sensor;
+	},
+	"getMarkers":function(model) {
+	    var attrs = ["movie","food"],
+	    marker_string = "";
+	    for(a in attrs) {
+		var loc = model.get(attrs[a]).location_specific,
+		symbol = attrs[a].substring(0,1).toUpperCase();
+		marker_string += "&markers=color:red|label:"+symbol+"|"+loc;
+	    }
+	    return marker_string;
 	},
 	"add":function(e) {
 	    
@@ -112,7 +159,18 @@ function create_suggestion_view(Y) {
     var attrs = {
 	"container":{
 	    "valueFn":function() {
-		
+		var a_main = Y.Node.create("<div class='accordion-group'/>"),
+		a_head = Y.Node.create("<div class='accordion-heading'/>"),
+		a_link = Y.Node.create("<a class='accordion-toggle' data-toggle='collapse'/>"),
+		a_body = Y.Node.create("<div class='accordion-body '/>"),
+		a_inner = Y.Node.create("<div class='accordion-inner'/>");
+		a_body.setAttribute("id",a_body.get("_yuid"));
+		a_link.setAttribute("href","#"+a_body.getAttribute("id"));
+		a_head.appendChild(a_link);
+		a_body.appendChild(a_inner);
+		a_main.appendChild(a_head);
+		a_main.appendChild(a_body);
+		return a_main;
 	    },
 	    "model":{
 		"value":null
@@ -125,20 +183,20 @@ function create_suggestion_view(Y) {
 function create_suggestion_view_list(Y) {
     var procs = {
 	"initializer":function(e) {
-	    var modellist = e.modellist;
+	    var modellist = e.modellist,
+	    srcnode = e.srcNode;
 	    this.set("modellist",modellist);
+	    this.set("srcNode",srcnode);
 	    modellist.on("add",this.add,this);
 	    modellist.on("remove",this.remove,this);
 	    modellist.on("reset",this.reset,this);
 	},
 	"render":function() {
 	    var modellist = this.get("modellist"),
-	    container = this.get("container");
-	    var build_view = function(model) {
-		var x_view = new Y.suggestion_view({"model":model});
-		container.append(x_view.render().get("container"));
-	    };
-	    modellist.each(build_view);
+	    container = this.get("container"),
+	    srcnode = this.get("srcNode");
+	    Y.one(srcnode).setContent(container);
+	    return this;
 	},
 	"add":function(e) {
 	    var container = this.get("container"),
@@ -146,19 +204,36 @@ function create_suggestion_view_list(Y) {
 	    container.append(x_view.render().get("container"));
 	},
 	"remove":function(e) {
-
+	    
 	},
 	"reset":function(e) {
 	    
+	},
+	"update":function() {
+	    var list = this.get("modellist"),
+	    container = this.get("container"),
+	    build_view = function(model,index) {
+		var x_view = new Y.suggestion_view({"model":model}),
+		content = x_view.render().get("container");
+		container.append(content);
+		container.one(".accordion-toggle").setAttribute("data-parent","#"+container.get("id"));
+	    };
+	    list.each(build_view);
 	}
+	
     };
     var attrs = {
 	"container":{
 	    "valueFn":function() {
-
+		var c = Y.Node.create("<div class='accordion'/>");
+		c.set("id",c.get("_yuid"));
+		return c;
 	    }
 	},
 	"modellist":{
+	    "value":null
+	},
+	"srcNode":{
 	    "value":null
 	}
     };
